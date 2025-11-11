0478e04e17c9750479de4a8104b6483127ce599f
EsmaNurOzkan <nuresmaozkan98@gmail.com>
2025-11-11 19:04:44 +0300

    expo version updated

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import 'react-native-get-random-values';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { auth } from './utils/firebaseConfig';
+
+import { auth, firestore } from './utils/firebaseConfig';
 
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
@@ -16,13 +17,32 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login"  options={{ title: 'Login' }} component={LoginScreen} />
-        <Stack.Screen name="Register" options={{ title: 'Register' }} component={RegisterScreen} />
-        <Stack.Screen name="Intermediate" options={{ title: ' ' }} component={IntermediateScreen} />
-        <Stack.Screen name="NewDiaryEntry" options={{ title: 'New diary' }} component={NewDiaryEntryScreen} />
-        <Stack.Screen name="PersonalDiary" options={{ title: 'My Diary' }} component={PersonalDiaryScreen} />
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen} 
+          options={{ title: 'Login' }} 
+        />
+        <Stack.Screen 
+          name="Register" 
+          component={RegisterScreen} 
+          options={{ title: 'Register' }} 
+        />
+        <Stack.Screen 
+          name="Intermediate" 
+          component={IntermediateScreen} 
+          options={{ title: ' ' }} 
+        />
+        <Stack.Screen 
+          name="NewDiaryEntry" 
+          component={NewDiaryEntryScreen} 
+          options={{ title: 'New diary' }} 
+        />
+        <Stack.Screen 
+          name="PersonalDiary" 
+          component={PersonalDiaryScreen} 
+          options={{ title: 'My Diary' }} 
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
- 
--- utils/firebaseConfig.js
+++ utils/firebaseConfig.js
@@ -1,20 +1,24 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'; 
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
 const firebaseConfig = {
-     apiKey: "your-api-key",
-     authDomain: "your-auth-domain",
-     projectId: "your-project-id",
-     storageBucket: "your-storage-bucket",
-     messagingSenderId: "your-messaging-sender-id",
-     appId: "your-app-id"
-   };
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "", 
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const firestore = getFirestore(app); 
 
-export { auth, firestore };
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+const firestore = getFirestore(app);
+
+export { app, auth, firestore };
